a7b21c13a44e6efd6023fa36918e56229f450819
rdenson <richard.denson@forgerock.com>
2024-06-24 15:14:32 -0500

    getting back to #11

--- install_forge.sh
+++ install_forge.sh
@@ -105,8 +105,8 @@ FORGE_BINARY="forge-${FORGE_VERSION}-${OS}"
 
 info "Installing ${FORGE_BINARY}..."
 
-curl -fL -# -H "${AUTH}" -o "${INSTALL_PATH}" "${REPO_URL}/${FORGE_BINARY}"
-chmod +x "${INSTALL_PATH}"
+sudo curl -fL -# -H "${AUTH}" -o "${INSTALL_PATH}" "${REPO_URL}/${FORGE_BINARY}"
+sudo chmod +x "${INSTALL_PATH}"
 
 cat << EOF
 ${BLUE}
